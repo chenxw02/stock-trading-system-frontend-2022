237411f0f17987144290df9b725971517daaeadf
TomisTony <1158176767@qq.com>
2022-05-13 01:51:49 +0800

    edit LoginPage

--- src/page/LoginPage/LoginPage.js
+++ src/page/LoginPage/LoginPage.js
@@ -1,5 +1,5 @@
 import "./LoginPage.css"
-import { Input, Button, Radio} from 'antd';
+import { Input, Button, Radio,Divider, Row, Col, Carousel, Image} from 'antd';
 import {
     UserOutlined,
     LockOutlined
@@ -7,7 +7,7 @@ import {
 import { useState} from "react";
 
 const options = [
-    { label: '普通用户', value: '普通用户' },
+    { label: '普通用户', value: '普通用户'},
     { label: '账户管理员', value: '账户管理员' },
     { label: '股票管理员', value: '股票管理员' },
   ];
@@ -15,52 +15,85 @@ const options = [
 
 function LoginPage() {
     const [identity, setIdentity] = useState("普通用户");
+    const contentStyle = {
+        height: '400px',
+        color: '#fff',
+        lineHeight: '360px',
+        textAlign: 'center',
+        background: '#364d79',
+      };
 
     return(
-        <div className="login_warning">
-            我先随手糊了一个方便大家开发，之后再美化（X）一下这玩意儿<br />
-            暂时不需要输入用户名和密码登录
-            <div className="login_border">
-                <Input
-                    className="login_inputbox"
-                    placeholder="用户名"
-                    prefix={<UserOutlined />}
-                />
-                <Input.Password
-                    className="login_inputbox"
-                    placeholder="密码"
-                    prefix={<LockOutlined />}
-                />
-                <Radio.Group 
-                    buttonStyle="solid"
-                    size="large"
-                    options={options}
-                    onChange = {(event) => { setIdentity(event.target.value) }}
-                    value={identity}
-                    optionType="button"
-                    className="login_group"
-                />
-                <div />
-                <Button  type="primary" className="login_button"
-                    onClick={()=>{
-                        switch(identity){
-                            case "普通用户":
-                                window.location.href="./trade";
-                                break;
-                            case "账户管理员":
-                                window.location.href="./money";
-                                break;
-                            case "股票管理员":
-                                window.location.href="./admin";
-                                break;
-                        }
-                    }}
-                > 登录</Button>
-                <Button  type="primary" className="login_button"
-                    onClick={()=>{window.location.href="./info";}}
-                > 游客访问</Button>
-                
-            </div>
+        <div className="login_background">
+            <Row>
+                <Col span={14}>
+                    <Carousel className="login_carousel">
+                        <div>
+                            <h1 style={contentStyle}>股票交易系统</h1>
+                        </div>
+                        <div>
+                            <h3 style={contentStyle}>A1 证券帐户业务</h3>
+                        </div>
+                        <div>
+                            <h3 style={contentStyle}>A2 资金帐户业务</h3>
+                        </div>
+                        <div>
+                            <h3 style={contentStyle}>A3 交易客户端</h3>
+                        </div>
+                        <div>
+                            <h3 style={contentStyle}>A4 中央交易系统业务</h3>
+                        </div>
+                        <div>
+                            <h3 style={contentStyle}>A5 交易系统管理业务</h3>
+                        </div>  
+                    </Carousel>
+                </Col>
+                <Col span={1}>
+                    <Divider type="vertical" className="login_divider" />
+                </Col>
+                <Col span={9}>
+                    <div className="login_border">
+                        <Input
+                            className="login_inputbox_user"
+                            placeholder="用户名"
+                            prefix={<UserOutlined />}
+                        />
+                        <Input.Password
+                            className="login_inputbox_pwd"
+                            placeholder="密码"
+                            prefix={<LockOutlined />}
+                        />
+                        <Radio.Group 
+                            buttonStyle="solid"
+                            size="large"
+                            options={options}
+                            onChange = {(event) => { setIdentity(event.target.value) }}
+                            value={identity}
+                            optionType="button"
+                            className="login_group"
+                        />
+                        <div />
+                        <Button  type="primary" className="login_button"
+                            onClick={()=>{
+                                switch(identity){
+                                    case "普通用户":
+                                        window.location.href="./trade";
+                                        break;
+                                    case "账户管理员":
+                                        window.location.href="./money";
+                                        break;
+                                    case "股票管理员":
+                                        window.location.href="./admin";
+                                        break;
+                                }
+                            }}
+                        > 登录</Button>
+                        <Button  type="primary" className="login_button"
+                            onClick={()=>{window.location.href="./info";}}
+                        > 游客访问</Button>
+                    </div>
+                </Col> 
+            </Row>  
         </div>
         
     )
