d0b5f660243480030a73446991905a4411a4de4a
TomisTony <1158176767@qq.com>
2022-05-12 14:18:52 +0800

    fix css and create child colome in AdminPage

--- src/page/AdminPage/AdminPage.js
+++ src/page/AdminPage/AdminPage.js
@@ -6,49 +6,88 @@ const data = [
     {
         id : '00000',
         name : '垃圾css有限公司',
-        info: "null"
+        price: 10,
+        number: 20098,
     },
     {
         id : '00001',
         name : '垃圾unity有限公司',
-        info: "null"
+        price: 5,
+        number: 12742,
     },
     {
         id : '00002',
         name : '垃圾vtk有限公司',
-        info: "null"
+        price: 2,
+        number: 109832,
     },
 ]
+
+const columns = [
+    {
+        title:"股票编号",
+        dataIndex:"id",
+        defaultSortOrder:"descend", 
+        sorter:"(a, b) => a.id - b.id",
+    },
+    {
+        title:"股票名称",
+        dataIndex:"name",
+        defaultSortOrder:"descend",
+        sorter:"(a, b) => a.name - b.name",
+    },
+    {
+        title:"股票信息",
+        children:[
+            {
+                title:"最新成交价格",
+                dataIndex:"price",
+                defaultSortOrder:"descend",
+                sorter:"(a, b) => a.price - b.price",
+            },
+            {
+                title:"最新成交数量",
+                dataIndex:"number",
+                defaultSortOrder:"descend",
+                sorter:"(a, b) => a.number - b.number",
+            },
+        ]
+    },
+    {
+        title:"Action",
+        key:"action",
+        render:() => (
+            <Space size="middle">
+            <a>详情</a>
+            </Space>
+        )
+    },
+  ];
 
 function AdminPage() {
     return(
         <div className='admin_background'>
 
         <div className='admin_header'>
-            Hi,Admin
-            <Button type="primary" danger className="admin_button"
-                onClick={()=>{
-                    window.location.href="./";
-                }}
-            >退出登录</Button>
-            <Button type="primary" className="admin_button">修改密码</Button>
+            <div className='admin_welcome'>Hi,Admin</div>
+
+            <div id="datetime" className="admin_datetime">  
+                {setInterval("document.getElementById('datetime').innerHTML=new Date().toLocaleString();", 1000)}  
+            </div>
+
+            <div className='admin_button_box'>
+               <Button type="primary" danger className="admin_button"
+                    onClick={()=>{
+                        window.location.href="./";
+                    }}
+                >退出登录</Button>
+                <Button type="primary" className="admin_button">修改密码</Button> 
+            </div>
+            
         </div>
 
         <div className='admin_table_back'>
-           <Table dataSource={data} className="admin_table">
-                <Column title="股票编号" dataIndex="id" defaultSortOrder="descend" sorter="(a, b) => a.id - b.id" />
-                <Column title="股票名称" dataIndex="name" defaultSortOrder="descend" sorter="(a, b) => a.name - b.name" />
-                <Column title="股票信息" dataIndex="info" defaultSortOrder="descend" sorter="(a, b) => a.info - b.info" />
-                <Column
-                title="Action"
-                key="action"
-                render={() => (
-                    <Space size="middle">
-                    <a>详情</a>
-                    </Space>
-                )}
-                />
-            </Table> 
+           <Table columns={columns} dataSource={data} className="admin_table" /> 
         </div>
         
         
