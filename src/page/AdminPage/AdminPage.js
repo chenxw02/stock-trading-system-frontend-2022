8edc7f4c9387af639528d244a1a60e4ae0ede34b
TomisTony <1158176767@qq.com>
2022-05-12 10:36:28 +0800

    小写了一波AdminPage

--- src/page/AdminPage/AdminPage.js
+++ src/page/AdminPage/AdminPage.js
@@ -1,8 +1,55 @@
+import './AdminPage.css';
+import { Table, Button, Space } from 'antd';
+const { Column } = Table;
+
+const data = [
+    {
+        id : '00000',
+        name : '垃圾css有限公司',
+        info: "null"
+    },
+    {
+        id : '00001',
+        name : '垃圾unity有限公司',
+        info: "null"
+    },
+    {
+        id : '00002',
+        name : '垃圾vtk有限公司',
+        info: "null"
+    },
+]
+
 function AdminPage() {
     return(
-        <div>
-            管理员
+        <div className='Admin_BackGound'>
+
+        <div className='Admin_Header'>
+            Hi,Admin
+            <Button type="primary" className="Admin_Button">修改密码</Button>
+            <Button type="primary" danger className="Admin_Button">退出登录</Button>
         </div>
+
+        <div className='Admin_TabelBack'>
+           <Table dataSource={data} className="Admin_Table">
+                <Column title="股票编号" dataIndex="id" defaultSortOrder="descend"/>
+                <Column title="股票名称" dataIndex="name" defaultSortOrder="descend" />
+                <Column title="股票信息" dataIndex="info" defaultSortOrder="descend" />
+                <Column
+                title="Action"
+                key="action"
+                render={() => (
+                    <Space size="middle">
+                    <a>详情</a>
+                    </Space>
+                )}
+                />
+            </Table> 
+        </div>
+        
+        
+        </div>
+        
     )
 }
 
