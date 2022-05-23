bf645297c2da51e4211d303395659254f66c1a28
TomisTony <1158176767@qq.com>
2022-05-23 14:18:03 +0800

    add requests.js

---
+++ src/utils/requests.js
@@ -0,0 +1,44 @@
+//TODO: 这里写网络请求函数
+const url_head = 'http://localhost:3000'
+
+/* 
+data指的是body的内容
+目前还没有和后端沟通好，所以header之类的请自己传入
+*/
+async function request(url = '', method = 'GET',header = '', data = {}) {
+    var final_url = url_head + url;
+    var response;
+    
+    switch(method.toLowerCase()){
+        case 'get':
+            response = await fetch(final_url, {
+                method: 'GET', 
+                headers: header,
+              });
+            break;
+        case 'post':            
+            response = await fetch(final_url, {
+                method: 'POST', 
+                headers: header,
+                body: JSON.stringify(data)
+              });
+            break;
+        case 'delete':
+            response = await fetch(final_url, {
+                method: 'DELETE',
+                headers: header, 
+              });
+            break;
+        case 'put':
+            response = await fetch(final_url, {
+                method: 'PUT', 
+                headers: header,
+                body: JSON.stringify(data)
+              });
+            break;
+    }
+    return response.json()
+}
+
+
+export default request;
