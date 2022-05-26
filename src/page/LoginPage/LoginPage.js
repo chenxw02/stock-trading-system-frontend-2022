b8adf4aac7ec6b40fbce324f53e27b6c9fb8c628
xp-dong <1457992388@qq.com>
2022-05-26 15:41:03 +0800

    登录

--- src/page/LoginPage/LoginPage.js
+++ src/page/LoginPage/LoginPage.js
@@ -91,8 +91,22 @@ function LoginPage() {
                                         window.location.href="./trade";
                                         break;
                                     case "账户管理员":
-                                        window.location.href="./stockadmin";
+                                        request('/account_admin/login', "POST",{'Content-Type': 'application/json'},
+                                        {
+                                            "administrator_id": aid,
+                                            "administrator_password": password
+                                        }).then((response) => {
+                                            console.log(response);
+                                            if (response.code == '0') {
+                                                window.location.href="./stockadmin";
+                                            }
+                                            else {
+                                            alert(response.message);
+                                            }
+                                        })
                                         break;
+                                        // window.location.href="./stockadmin";
+                                        // break;
                                     case "股票管理员":
                                         request('/admin/login', "POST",{'Content-Type': 'application/json'},
                                         {
